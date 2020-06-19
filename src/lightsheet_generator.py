f17b51cd838b27eeb54e13ad4c6e2c62a5851226
Nikita Vladimirov <nvladimus@gmail.com>
2020-06-19 16:11:10 +0200

    all modules work, battle test todo

--- src/lightsheet_generator.py
+++ src/lightsheet_generator.py
@@ -35,7 +35,8 @@ class LightsheetGenerator(QtCore.QObject):
         super().__init__()
         self.config = config
         self.daqmx_task = self.serial_arduino = None
-        self.initialized = self.ls_active = False
+        self.initialized = False
+        self.status = "OFF"
         # logger setup
         self.logger_name = logger_name
         self.logger = logging.getLogger(logger_name)
@@ -66,6 +67,9 @@ class LightsheetGenerator(QtCore.QObject):
         if self.daqmx_task:
             self.cleanup_daqmx_task()
             self.logger.info('DAQmx task closed.')
+            self.status = "OFF"
+            self.initialized = False
+            if self.gui_on: self.sig_update_gui.emit()
 
     def create_daqmx_task(self):
         """Create the DAQmx task, but don't start it yet."""
@@ -121,6 +125,11 @@ class LightsheetGenerator(QtCore.QObject):
                 self.serial_arduino.write('v1 0.0\n'.encode())
                 self.serial_arduino.write('reset\n'.encode())
 
+    def set_switching_period(self, n: int):
+        self.config['switch_every_n_pulses'] = n
+        self.setup_arduino()
+        if self.gui_on: self.sig_update_gui.emit()
+
     def setup_ls(self):
         """Set up the lightsheet DAQmx task. """
         assert self.config['laser_pow_volts'] <= self.config['laser_max_volts'], 'Laser voltage too high'
@@ -135,6 +144,9 @@ class LightsheetGenerator(QtCore.QObject):
                                  laser_amplitude_V=self.config['laser_pow_volts'],
                                  galvo_inertia_ms=0.2)
                 self.logger.info('DAQmx AO task configured.')
+                self.status = "ON"
+                if self.gui_on: self.sig_update_gui.emit()
+                self.initialized = True
             except pd.DAQException as e:
                 self.logger.error(f"Config DAQmx: {e.message}")
         else:
@@ -196,6 +208,7 @@ class LightsheetGenerator(QtCore.QObject):
         tab_name = 'LS settings'
         self.gui.add_button('Initialize', tab_name, func=self.initialize)
         self.gui.add_string_field('Port', tab_name, value=self.config['arduino_switcher_port'], enabled=False)
+        self.gui.add_string_field('Status', tab_name, value=self.status, enabled=False)
         self.gui.add_numeric_field('Swipe duration', tab_name, value=self.config['swipe_duration_ms'],
                                    vmin=0.1, vmax=100, decimals=1,
                                    func=partial(self.update_config, 'swipe_duration_ms'))
@@ -232,52 +245,8 @@ class LightsheetGenerator(QtCore.QObject):
     @QtCore.pyqtSlot()
     def _update_gui(self):
         self.gui.update('Active arm', self.config['active_arm'])
-
-
-def task_config(task, wf_duration_ms=50,
-                   galvo_offset_V=0,
-                   galvo_amplitude_V=1.0,
-                   laser_amplitude_V=0.0,
-                   galvo_inertia_ms=0.20):
-    """Configuration and automatic restart of light-sheet generation DAQmx AO task.
-    Channels:
-        ao0, galvo
-        ao1, laser
-    Parameters:
-        task, existing DAQmx AO task
-        wf_duration_ms
-        galvo_offset_V
-        galvo_amplitude_V
-        laser_amplitude_V.
-        galvo_inertia_ms, delay in laser onset after galvo, to accomodate galvo inertia.
-    """
-    task.StopTask()
-    sampleRate_Hz = 20000
-    samples_per_ch = np.int(sampleRate_Hz/1000. * wf_duration_ms)
-    task.CfgSampClkTiming("", sampleRate_Hz, pd.DAQmx_Val_Rising, pd.DAQmx_Val_FiniteSamps, samples_per_ch)
-
-    task.CfgDigEdgeStartTrig("/Dev1/PFI0", pd.DAQmx_Val_Rising)
-    task.SetTrigAttribute(pd.DAQmx_StartTrig_Retriggerable, True)
-
-    # generate galvo AO waveform
-    wf_galvo = np.zeros(samples_per_ch)
-    wf_sawtooth = np.linspace(-galvo_amplitude_V/2.0, galvo_amplitude_V/2.0, samples_per_ch-2)
-    # note that the last value of waveform should be zero or galvo_offset_V constant
-    wf_galvo[1:-1] = wf_sawtooth
-    wf_galvo = wf_galvo + galvo_offset_V
-    # generate laser ON/OFF waveform
-    wf_laser = np.zeros(samples_per_ch)
-    laser_delay_samples = int(sampleRate_Hz/1000. * galvo_inertia_ms)
-    wf_laser[laser_delay_samples:-1] = laser_amplitude_V  # laser wf must end with zero for safety reasons.
-    # combine
-    wform2D = np.column_stack((wf_galvo,wf_laser))
-    # write to buffer
-    samples_per_ch_ct = ct.c_int32()
-    samples_per_ch_ct.value = samples_per_ch
-    task.WriteAnalogF64(samples_per_ch, False, 10, pd.DAQmx_Val_GroupByScanNumber,
-                        wform2D, ct.byref(samples_per_ch_ct), None)
-    # restart the task
-    task.StartTask()
+        self.gui.update('Switch every N pulses', self.config['switch_every_n_pulses'])
+        self.gui.update('Status', self.status)
 
 
 # run if the module is launched as a standalone program
